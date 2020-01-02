b7d224f0fe3accf184cf0a98061538fbb5c59543
Tomasz Piątek <tomasz.piatek@sts.local>
2020-01-02 14:35:52 +0100

    Add account balance and nav in header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './nav/nav.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, BrowserAnimationsModule],
     declarations: [HeaderComponent, FooterComponent, NavComponent],
     exports: [HeaderComponent, FooterComponent, NavComponent]
 })
