128ed8d62ed3fec4983e49b7c3834461bece0f7e
Tomasz Piątek <tomasz.piatek@sts.local>
2020-03-15 09:28:28 +0100

    List

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -4,14 +4,36 @@ import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from '../shared/shared.module';
 import { ListComponent } from './list/list.component';
-import {NgxDatatableModule} from '@swimlane/ngx-datatable';
 import { FiltersComponent } from './filters/filters.component';
-import {TableModule} from 'primeng/table';
+import { TableModule } from 'primeng/table';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { IncomesComponent } from './incomes/incomes.component';
+import { RefillComponent } from './refill/refill.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { ListDirective } from './list/list-directive';
 
 @NgModule({
-    imports: [CommonModule, RouterModule, FontAwesomeModule, SharedModule, TableModule],
-    declarations: [ListComponent, FiltersComponent, PieChartComponent, BarChartComponent]
+    imports: [
+        CommonModule,
+        RouterModule,
+        FontAwesomeModule,
+        SharedModule,
+        TableModule
+    ],
+    declarations: [
+        ListDirective,
+        ListComponent,
+        FiltersComponent,
+        PieChartComponent,
+        BarChartComponent,
+        IncomesComponent,
+        RefillComponent,
+        ExpensesComponent
+    ],
+    entryComponents: [
+        IncomesComponent,
+        ExpensesComponent,
+    ]
 })
 export class ListModule {}
--- src/app/list/list/list.component.ts
+++ src/app/list/list/list.component.ts
@@ -1,94 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ComponentFactoryResolver,
+    OnInit,
+    ViewChild,
+    ViewContainerRef
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ExpenseService } from '../../services/expense.service';
-import { IncomeService } from '../../services/income.service';
-import { switchMap, map } from 'rxjs/operators';
-import * as dayjs from 'dayjs';
-import Swal from 'sweetalert2';
-import {ICategory} from '../../interfaces/category';
-import {CategoryService} from '../../services/category.service';
-import {combineLatest} from 'rxjs';
+import { ListDirective } from './list-directive';
+import { ExpensesComponent } from '../expenses/expenses.component';
+import { IncomesComponent } from '../incomes/incomes.component';
+import { RefillComponent } from '../refill/refill.component';
 
 @Component({
     selector: 'app-list',
     templateUrl: './list.component.html',
     styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
-    loading = true;
-    categories$;
-    categories = [];
-    tableData = [];
-    barChartData = [];
+export class ListComponent implements OnInit, AfterViewInit {
     title = '';
+    @ViewChild(ListDirective) appListComponent: ListDirective;
+
     constructor(
         private route: ActivatedRoute,
-        private expenseService: ExpenseService,
-        private incomeService: IncomeService,
-        private categoryService: CategoryService
+        private componentFactoryResolver: ComponentFactoryResolver
     ) {}
 
-    ngOnInit(): void {
-        this.route.paramMap
-            .pipe(switchMap((params: any) => this.getList(params)))
-            .subscribe(
-                (data: any) => {
-                    this.loading = false;
-                    this.tableData = data.list;
-                    this.getCategories();
-                },
-                err => {
-                    this.loading = false;
-                    this.tableData = [];
-                    Swal.fire({
-                        title: 'Błąd pobierania danych!',
-                        text:
-                            'Wystąpił błąd z pobraniem danych. Sprawdź połączenie lub spróbuj ponownie później.',
-                        icon: 'error'
-                    });
-                }
+    ngOnInit(): void {}
+
+    ngAfterViewInit(): void {
+        setTimeout(() => {
+            this.route.paramMap.subscribe((data: any) =>
+                this.getList(data.params)
             );
+        });
     }
 
-    getList({ params }) {
-        let filters = {};
-        this.route.queryParamMap.subscribe((query: any) => {
-            filters = query.params;
-        });
-        this.loading = true;
-        switch (params.type) {
+    getList({ type }) {
+        switch (type) {
             case 'expenses':
                 this.title = 'Wydatki';
-                this.getBalanceInMonths('expenses', this.expenseService.getExpensesInMonths());
-                return this.expenseService.getExpenses(filters);
+                return this.getListComponent(type);
             case 'incomes':
-                this.title = 'Przychody'
-                this.getBalanceInMonths('incomes', this.incomeService.getIncomesInMonths());
-                return this.incomeService.getIncomes(filters);
+                this.title = 'Przychody';
+                return this.getListComponent(type);
+            case 'refill':
+                this.title = 'Tankowanie';
+                return this.getListComponent(type);
         }
     }
 
-    parseDate(date) {
-        return dayjs(date).format('DD/MM/YYYY');
-    }    
-    
-    getCategories(): void {
-        this.categories$ = this.categoryService
-            .findAll()
-            .pipe(map(categories => categories.map(item => {
-                item.sum = 0;
-                item.expenses.map(exp => item.sum += Math.floor(exp.value));
-                return item;
-            })))
-            .subscribe((data: any) => {
-                console.log(data);
-                this.categories = data;
-            });
+    getListComponent(pageType: string) {
+        const component = this.setChoosenComponent(pageType);
+        if (this.appListComponent != null) {
+            component
+                ? this.createComponentView(
+                      component,
+                      this.appListComponent.viewContainerRef
+                  )
+                : this.appListComponent.viewContainerRef.clear();
+        }
     }
 
-    getBalanceInMonths(key, service) {
-        service.subscribe((data: any) => {
-            this.barChartData = data;
-        });
+    private setChoosenComponent(pageType: string): any {
+        switch (pageType) {
+            case 'expenses':
+                return ExpensesComponent;
+            case 'incomes':
+                return IncomesComponent;
+            case 'refill':
+                return RefillComponent;
+        }
+    }
+
+    private createComponentView(
+        component: any,
+        viewContainerRef: ViewContainerRef
+    ): void {
+        viewContainerRef.clear();
+        const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+            component
+        );
+        viewContainerRef.createComponent(componentFactory);
     }
 }
